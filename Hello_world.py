2e19b76ae3bbf2692ed608d6842c155acd49a7b9
Sai Nagavarapu <sainagavarapu@Sais-MacBook-Air.local>
2026-06-09 15:37:18 -0700

    Add my changes

--- Hello_world.py
+++ Hello_world.py
@@ -1,1 +1,19 @@
+<<<<<<< ours
 print("hello world")
+=======
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(b"Hello World")
+
+
+if __name__ == "__main__":
+    server = HTTPServer(("127.0.0.1", 8000), HelloHandler)
+    print("Server running at http://127.0.0.1:8000")
+    server.serve_forever()
+>>>>>>> theirs
